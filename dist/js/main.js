5e97851e7bd6602a24669571cfc93632ac935291
patrickfeitosa <31283687+patrickfeitosa@users.noreply.github.com>
2018-05-30 16:41:59 -0300

    Implementação do menu mobile junto da searchbox
    Implementação do menu mobile junto da searchbox

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,37 @@
-"use strict";var onReady=function(){fetch("http://localhost:3000/json/potions.json").then(function(o){return o.json()}).then(function(o){console.log(o)}).catch(function(o){console.error("Erro ao recuperar os dados do JSON",o)})};"loading"!==document.readyState&&onReady(),document.addEventListener("DOMContentLoaded",onReady);
+'use strict';
+
+var onReady = function onReady() {
+	var url = "http://localhost:3000/json/potions.json";
+	fetch(url).then(function (response) {
+		return response.json();
+	}) // retorna uma promise
+	.then(function (result) {
+		console.log(result);
+	}).catch(function (err) {
+		// trata se alguma das promises falhar
+		console.error('Erro ao recuperar os dados do JSON', err);
+	});
+
+	var btnMobile = document.querySelector('#mobile-btn');
+	btnMobile.addEventListener('click', function (btn) {
+		enableMenu('.menu');
+	});
+};
+
+if (document.readyState !== 'loading') {
+	onReady();
+}
+
+document.addEventListener('DOMContentLoaded', onReady);
+
+var enableMenu = function enableMenu(targetClick) {
+	var menu = document.querySelector(targetClick);
+	var searchBox = document.querySelector('#search-box');
+	var logo = document.querySelector('.logo');
+	var bag = document.querySelector('.bag-info');
+
+	menu.classList.toggle('d-sm-none');
+	searchBox.classList.toggle('d-sm-none');
+	logo.classList.toggle('d-sm-none');
+	bag.classList.toggle('d-sm-none');
+};
